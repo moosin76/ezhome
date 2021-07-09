8b33d8c411034ff8e048bcfcb5302fe60a9429c4
Kiyong Nam <moosin76@gmail.com>
2021-07-09 15:08:20 +0900

    배포 테스트

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,9 @@
 <template>
-  <hello-world />
+  <div>배포테스트</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: "Home",
+};
 </script>
