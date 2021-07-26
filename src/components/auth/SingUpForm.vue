39bde94e8053122f4b8a8a65da5204d01f79f129
Kiyong Nam <moosin76@gmail.com>
2021-07-26 16:02:58 +0900

    022 회원가입 레이아웃

---
+++ src/components/auth/SingUpForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form @submit.prevent="save" ref="form" v-model="valid" lazy-validation>
+    <v-btn type="submit" block color="primary">회원가입</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "SignUpForm",
+  data() {
+    return {
+      valid: true,
+      form: {
+        mb_id: "",
+        mb_password: "",
+        mb_name: "",
+        mb_birth: "",
+        mb_gender: "",
+        mb_email: "",
+        mb_phone: "",
+        mb_zip: "",
+        mb_addr1: "",
+        mb_addr2: "",
+      },
+    };
+  },
+  methods: {
+    save() {
+      console.log(this.form);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/member/Join.vue
+++ src/views/member/Join.vue
@@ -1,13 +1,23 @@
 <template>
-	<div>Join</div>
+  <div class="d-flex justify-center align-center" style="height:100%">
+    <v-card max-width="400" width="100%" elevation="10">
+      <v-toolbar>
+        <v-toolbar-title>회원가입</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text> 
+				<sing-up-form/> 
+			</v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import SingUpForm from "../../components/auth/SingUpForm.vue";
 export default {
-
-}
+  components: { SingUpForm },
+  name: "Join",
+};
 </script>
 
 <style>
-
 </style>
